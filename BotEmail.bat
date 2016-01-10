f4d06c292f27dd0005840d0ab0581b97ca741417
Darren357 <dlmccaul@mtu.edu>
2016-01-10 10:18:10 -0500

    finishing touches

--- BotEmail.bat
+++ BotEmail.bat
@@ -4,8 +4,8 @@ cd C:\Darren\github\emailpage
 for /f "tokens=*" %%a in ('time /t') do set time=%%a
 for /f "tokens=2" %%a in ('date /t') do set date=%%a
 set datetime=%date% %time%
-::if exist datetime.js del datetime.js
-::echo TargetDate = "%datetime%";>datetime.js
+if exist datetime.js del datetime.js
+echo TargetDate = "%datetime%";>datetime.js
 timeout /t 1 >nul
 git add datetime.js
 git commit -m "Beep Boop"
